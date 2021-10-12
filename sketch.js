636d6bb85fee276b241c6d0338c049fe6ac4b2c9
Reinald Van Praet <reinald@fabrique.nl>
2021-10-12 11:46:59 +0200

    setup fading transition

--- sketch.js
+++ sketch.js
@@ -7,14 +7,12 @@ let origH
 let imgAR
 let maskSize = 600
 let flipped = false
-let flipping = false // Sets true when user clicks and swaps image
-let flipCounter = 0
-let flipIncrement = 0
-let flipDuration = 1000
-let flipD = 0
-let fading = false // After swapping image, the masking lens is faded back in
-let fadeCounter = 0
-let fadeDuration = 500
+let fadingOut = false // After swapping image, the masking lens is faded back in
+let fadeOutCounter = 0
+let fadeOutId
+let fadingIn = false
+let fadeInCounter = 0
+let fadeInId
 let isResizing = false
 let debounceId = null
 let fr
@@ -38,45 +36,43 @@ function setup() {
 }
 
 function draw() {
-	!flipping && image(mainImg, 0, 0)
-	if (mouseInBounds()) {
-		if (!flipping && !fading) {
-			drawMask(maskSize)
+	push()
+	if (!fadingOut) {
+		image(mainImg, 0, 0)
+		!mouseOutBounds() && drawMask(maskSize)
+	} else {
+		if (fadeOutCounter >= 50) {
+			clearInterval(fadeOutId)
+			fadingOut = false
+			fadeOutCounter = 0
+			swapImages()
+			fadingIn = true
+			fadeInId = setInterval(() => {
+				fadeInCounter += 10
+			})
 		}
-		if (flipping && !fading) {
-			console.log('Flipping')
-			flipCounter += flipIncrement
-			drawMask(maskSize + flipCounter)
-			if (flipCounter - 1.5 * maskSize > flipD) {
-				console.log('Swapping images :::')
-				console.log('flipCounter :::', flipCounter)
-				console.log('flipD :::', flipD)
-				swapImages()
-			}
-		}
-		if (!flipping && fading) {
-			fadeCounter += 13.33 * 1.5
-			drawMask(1 + fadeCounter)
-		}
+		tint(255, fadeOutCounter)
+		image(maskImg, 0, 0)
 	}
-
+	pop()
 	fr.html(floor(frameRate()))
 }
 
 function mousePressed() {
-	if (flipping || fading) return
-	if (!mouseInBounds()) return
-	flipD = getFurthestFlipCorner()
-	flipIncrement = getFlipIncrement(flipD)
-	flipping = true
-	requestPointerLock()
+	if (fadingOut) return
+	if (!mouseOutBounds()) {
+		fadingOut = true
+		fadeOutId = setInterval(() => {
+			fadeOutCounter++
+		}, 1)
+	}
 }
 
 function windowResized() {
-	if (debounceId !== null) clearTimeout(debounceId)
-	debounceId = setTimeout(() => {
+	// if (debounceId !== null) clearTimeout(debounceId)
+	// debounceId = setTimeout(() => {
 		onResize()
-	}, 250)
+	// }, 250)
 }
 
 /**
@@ -96,29 +92,25 @@ function drawMask(r) {
 	circleMask.noStroke()
 	const cycle = frameCount / 200
 
-	if (flipping) {
-		circleMask.fill(0, 0, 0, 255)
-		circleMask.circle(floor(r/2), floor(r/2), r)
-	} else {
-		const newR = r - 100
-		const coef = flipping ? 96 : 12
-		let counter = 0
-		// Draw circles with different alpha values and animate them
-		for (let x = newR; x > 0; x -= coef) {
-			const t = floor(coef / 2) // Translate value
-			const a = (flipping || fading ? easeOutCirc(1 - x / newR) : easeOutBounce(1 - x / newR)) * 255 // Alpha value
-			const pulseOff = counter%2 == 0 ? sin(cycle) : cos(cycle)
-			const radius = x + 20 * pulseOff
 
-			// Draw on mask graphic
-			push()
-			circleMask.translate(t, t)
-			circleMask.fill(0, 0, 0, a)
-			circleMask.ellipse(floor(x/2), floor(x/2), radius + 20 * sin(cycle), radius + 20 * cos(cycle))
-			pop()
+	const newR = r - 100
+	const coef = 12
+	let counter = 0
+	// Draw circles with different alpha values and animate them
+	for (let x = newR; x > 0; x -= coef) {
+		const t = floor(coef / 2) // Translate value
+		const a = easeOutBounce(1 - x / newR) * 255 // Alpha value
+		const pulseOff = counter%2 == 0 ? sin(cycle) : cos(cycle)
+		const radius = x + 20 * pulseOff
 
-			counter++
-		}
+		// Draw on mask graphic
+		push()
+		circleMask.translate(t, t)
+		circleMask.fill(0, 0, 0, a)
+		circleMask.ellipse(floor(x/2), floor(x/2), radius + 20 * sin(cycle), radius + 20 * cos(cycle))
+		pop()
+
+		counter++
 	}
 
 	// Cut masking image
@@ -130,8 +122,16 @@ function drawMask(r) {
 	// Translate to put cursor in the middle
 	push()
 	translate(r/2, r/2)
+	// fadingOut && tint(255, 1 / fadeOutCounter)
+	// fadingIn && tint(255, fadeInCounter)
 	image(img, xStart, yStart)
 	pop()
+	if (fadeInCounter >= 255) {
+		fadingIn = false
+		fadeInCounter = 0
+		flipped = !flipped
+		clearInterval(fadeInId)
+	}
 	circleMask.remove()
 }
 
@@ -165,15 +165,6 @@ function swapImages() {
 	const maskCopy = maskImg
 	maskImg = mainCopy
 	mainImg = maskCopy
-	flipping = false
-	fading = true
-	flipCounter = 0
-	exitPointerLock()
-	setTimeout(() => {
-		fading = false
-		flipped = !flipped
-		fadeCounter = 0
-	}, fadeDuration)
 }
 
 // Helper Functions
@@ -184,37 +175,6 @@ function getResizeDim() {
 	return { w, h }
 }
 
-function mouseInBounds() {
-	return mouseX < width || mouseX > width || mouseY < height || mouseY > height
-}
-
-function getFurthestFlipCorner() {
-	const x = mouseX
-	const y = mouseY
-	let result
-	if (x <= width/2 && y <= height/2) {
-		console.log('Scenario 1')
-		result = dist(x, y, width, height)
-	}
-	if (x > width/2 && y < height/2) {
-		console.log('Scenario 2')
-
-		result = dist(x, y, 0, height)
-	}
-	if (x < width/2 && y > height/2) {
-		console.log('Scenario 3')
-
-		result = dist(x, y, width, 0)
-	}
-	if (x > width/2 && y > height/2) {
-		console.log('Scenario 4')
-		result = dist(0, 0, x, y)
-	}
-	console.log('result ::: ', result)
-	return result
-}
-
-function getFlipIncrement(r) {
-	console.log('flipIncrement ::: ' ,(r / 30) * (1000 / flipDuration))
-	return (r / 20) * (1000 / flipDuration)
+function mouseOutBounds() {
+	return mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height
 }
